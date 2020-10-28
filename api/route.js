8d89c5ddad90f6dc2bb87edc1f2f494e9d2f7f1b
Noah Greff <greffnoah@gmail.com>
2020-10-28 17:01:33 +0100

    adding updated api/routes post metohd

--- api/route.js
+++ api/route.js
@@ -10,7 +10,7 @@ module.exports = router;
 
 
 
-router.post('/', async(req, res) => {
+router.post('/create', async(req, res) => {
 
     let email = req.body.email;
     let password = req.body.password;
