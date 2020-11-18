5c38a7443677659be1a6e3de48f96d31ed657064
Noah Greff <greffnoah@gmail.com>
2020-11-18 02:52:29 +0100

    .

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -144,7 +144,7 @@ function find(params) {
 function update(params) {
     return new Promise(async(resolve, reject) => {
 
-        consolg.log(params);
+        console.log(params);
         
         await validator.make(params.find).catch(err => reject(err));
         await validator.make(params.with).catch(err => reject(err));
